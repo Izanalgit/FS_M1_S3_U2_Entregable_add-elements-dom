e92b93a62e1e753123ea8ea9153840cda4f49c95
Izanalgit <izanalvarez@gmail.com>
2024-02-20 15:49:19 +0100

    Resuelto sin css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+
+ const listEl = document.getElementById("lista");
+ const butoEl = document.getElementById("agregar");
+
+ butoEl.addEventListener("click",function(){
+    let newEl =prompt("Introduce un nuevo elemento:");
+    let addlist = document.createElement("li");
+
+    addlist.textContent=newEl;
+
+    listEl.appendChild(addlist);
+ });
+ 
