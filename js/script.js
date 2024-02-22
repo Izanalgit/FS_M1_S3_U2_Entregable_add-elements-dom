cf3bbfcc0fefa71b14c185e74c4d751cf0424b7e
Izanalgit <izanalvarez@gmail.com>
2024-02-22 02:46:20 +0100

    JS pulido, CSS añadido

--- js/script.js
+++ js/script.js
@@ -3,12 +3,36 @@
  const listEl = document.getElementById("lista");
  const butoEl = document.getElementById("agregar");
 
+ let listado = [];
+ 
+ function addElemento (padre,etiqueta,lista){
+   padre.innerHTML = "";
+
+   for (let newEl of lista){
+      let addlist = document.createElement(etiqueta);
+      addlist.textContent=newEl;
+
+      padre.appendChild(addlist);
+   }
+   
+ }
+
  butoEl.addEventListener("click",function(){
     let newEl =prompt("Introduce un nuevo elemento:");
-    let addlist = document.createElement("li");
 
-    addlist.textContent=newEl;
+    if (newEl.trim()===""){
+      console.log("campo vacio");
+    }else{
+      listado.push(newEl);
+      /* FORMA NORMAL Y LOGICA
+      let addlist = document.createElement("li");
+      addlist.textContent=newEl;
 
-    listEl.appendChild(addlist);
+      listEl.appendChild(addlist);
+      */
+
+    }
+    // FORMA "IMPLEMENTAR CON CALZADOR UN ARRAY"
+    addElemento(listEl,"li",listado);
  });
  
